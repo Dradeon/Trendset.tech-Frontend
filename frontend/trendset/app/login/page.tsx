6794b9e1ef33a8a1098267164dbad46a69996a91
clivinghouse <79345446+clivinghouse@users.noreply.github.com>
2023-10-22 13:02:32 -0400

    made login pretty

--- frontend/trendset/app/login/page.tsx
+++ frontend/trendset/app/login/page.tsx
@@ -34,20 +34,21 @@ const login = () => {
   }
 
   return (
-    <div>
+    
+    <div className="max-h-full">
         <nav className="flex justify-between">
           <a href='/'><h1 className='text-4xl font-bold'>TrendSet.tech</h1></a>
         </nav>
-        <div className='mt-4 mb-4 flex flex-col'>
+        <div className='mt-4 mb-4 flex flex-col border-2 rounded max-w-xs p-2 mx-auto align-middle'>
           <h1 className='text-4xl font-bold text-center mt-8 mb-8'>Login</h1>
           <form onSubmit={login} className='m-auto mb-6'>
               <label htmlFor='email'>
                   <p>Email:</p>
-                  <input className='border mb-4' required type='email' name='email' id='email' onChange={(e)=>setEmail(e.target.value)}></input>
+                  <input className='shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline' required type='email' name='email' id='email' onChange={(e)=>setEmail(e.target.value)}></input>
               </label>
               <label htmlFor='password'>
                   <p>Password:</p>
-                  <input className='border mb-4' required type='password' name='email' id='password' onChange={(e) => setPassword(e.target.value)}></input>
+                  <input className='shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline' required type='password' name='email' id='password' onChange={(e) => setPassword(e.target.value)}></input>
               </label>
               <div className='flex justify-center'>
                 <button type="submit" className='pt-2 pb-2 pl-4 pr-4 self-center bg-green-500 font-bold rounded-md'>Login</button>
