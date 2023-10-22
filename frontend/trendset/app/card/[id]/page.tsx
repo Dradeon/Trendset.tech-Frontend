1b9c88c9a25b0bfe5380e93c1d7a00b11c39952b
Dradeon <69202556+Dradeon@users.noreply.github.com>
2023-10-22 13:34:22 -0400

    Changed Card page

--- frontend/trendset/app/card/[id]/page.tsx
+++ frontend/trendset/app/card/[id]/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-import Image from 'next/image'
 import { useQuery } from 'react-query'
 import { notFound } from 'next/navigation'
 import BASE_URL from '@/utils/constants'
@@ -7,7 +6,7 @@ import BASE_URL from '@/utils/constants'
 interface dataResponse {
     product:{
         id: number,
-        logo: string,
+        Logo: string,
         company_name: string,
         bio: string,
         links: {
@@ -45,19 +44,19 @@ export default function Page({ params }: { params: { id: string } }) {
 
     return <div className='m-auto text-center' style={{color:`${data.design.card_color}`}}>
         <div className='mt-8 mb-8'>
-            <Image src={data.product.logo} alt='Business Logo'></Image>
-            <h1 className={`decoration-[${data.design.text_color}] font-bold`}>{data.product.company_name}</h1>
+            <img src={data.product.Logo} width={200} height={200} className='mx-auto rounded-full' alt='Business Logo'></img>
+            <h1 className={`decoration-[${data.design.text_color}] text-4xl my-4 font-bold`}>{data.product.company_name}</h1>
             <p>{data.product.bio}</p>
-            <div className={`decoration-[${data.design.text_color}]`}>Links</div>
+            <div className={`decoration-[${data.design.text_color}] my-4`}>Links</div>
             {
                 data.product.links.map((link) => {
-                    return <button className='border-8 p-8 rounded-md'><a href={link.url}>{link.text}</a></button>
+                    return <button className='border-4 border-black px-8 py-3 rounded-lg my-3'><a target='_blank' href={link.url}>{link.text}</a></button>
                 })
             }
         </div>
         <div>
             <p>Like this card?</p>
-            <a href="/sign_up">Create your own Marketing Card today</a>
+            <a  className='underline' href="/sign_up">Create your own Card on TrendSet</a>
         </div>
     </div>
 }
